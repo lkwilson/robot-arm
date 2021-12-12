695e218f1beff96145e9b7a7d19ab1fbc98b63e3
Lucas Wilson <lkwilson96@gmail.com>
2021-12-12 14:40:25 -0700

    Add unit tests

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,5 +1,9 @@
 import numpy as np
+import logging
 from robot_arm.transforms import RobotState, ArmPoint, rot_x, rot_y, rot_z, down_arm, up_arm
+from scipy.spatial.transform import Rotation
+
+logger = logging.getLogger(__name__)
 
 '''
 class RobotState:
@@ -28,19 +32,49 @@ def get_state():
     thetas = np.zeros(6)
   )
 
+def rot_check(rot, x, exp):
+  res =rot @ x
+  logger.info(res)
+  assert np.all(np.isclose(res, exp))
+
 def test_rot_x():
-  assert False
+  x_hat = np.array([1, 0, 0])
+  for theta in np.arange(start=-20, stop=20, step=.05):
+    rot = Rotation.from_rotvec(theta * x_hat)
+    logger.info("%s", rot.as_matrix())
+    assert np.all(np.isclose(rot.as_matrix(), rot_x(theta)))
 
 def test_rot_y():
-  assert False
+  x_hat = np.array([0, 1, 0])
+  for theta in np.arange(start=-20, stop=20, step=.05):
+    rot = Rotation.from_rotvec(theta * x_hat)
+    logger.info("%s", rot.as_matrix())
+    assert np.all(np.isclose(rot.as_matrix(), rot_y(theta)))
 
 def test_rot_z():
-  assert False
+  x_hat = np.array([0, 0, 1])
+  for theta in np.arange(start=-20, stop=20, step=.05):
+    rot = Rotation.from_rotvec(theta * x_hat)
+    logger.info("%s", rot.as_matrix())
+    assert np.all(np.isclose(rot.as_matrix(), rot_z(theta)))
 
-def test_down_arm():
+def test_traverse_arm():
   ''' def down_arm(state: RobotState, arm_point: ArmPoint): '''
-  assert False
+  state = get_state()
+  init_point = ArmPoint(0, np.zeros(3))
+  point = init_point
+  for i in range(6):
+    last_point = point
+    point = down_arm(state, point)
+    assert point.index == last_point.index + 1
+    logger.info('%s -> %s', last_point, point)
+  assert point.index == 6
+  #assert np.all(np.isclose(point.point, []))
 
-def test_down_arm():
-  ''' def up_arm(state: RobotState, arm_point: ArmPoint): '''
-  assert False
+  for i in range(6):
+    last_point = point
+    point = up_arm(state, point)
+    assert point.index == last_point.index - 1
+    logger.info('%s -> %s', last_point, point)
+  assert init_point.index == point.index
+  assert np.all(np.isclose(point.point, init_point.point))
