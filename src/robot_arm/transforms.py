c17661df72c4dd6231a7cccbc636ed44bdfe56fd
Lucas Wilson <lkwilson96@gmail.com>
2021-12-12 19:18:54 -0700

    add docs

--- src/robot_arm/transforms.py
+++ src/robot_arm/transforms.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+
+'''  rot mat builders '''
+
 def build_rot_x(theta: float):
   return np.array([
     [1, 0, 0],
@@ -21,23 +24,40 @@ def build_rot_z(theta: float):
     [0, 0, 1],
   ])
 
-def down_arm_single_pt(points, rot, rot_z, tran):
-  return down_arm(points[:, None], rot, rot_z, tran).reshape(-1)
+
+''' helper functions for demo purposes really '''
+
+def down_arm_single_pt(point, rot, rot_z, tran):
+  ''' point has shape (3, ) '''
+  return down_arm(point[:, None], rot, rot_z, tran).reshape(-1)
 
 def down_arm_multi_pt(points, rot, rot_z, tran):
+  ''' points has shape (n, 3) '''
   return down_arm(points.T, rot, rot_z, tran).T
+
+def up_arm_single_pt(point, rot, rot_z, tran):
+  ''' points has shape (3, ) '''
+  return up_arm(point[:, None], rot, rot_z, tran).reshape(-1)
+
+def up_arm_multi_pt(points, rot, rot_z, tran):
+  ''' points has shape (n, 3) '''
+  return up_arm(points.T, rot, rot_z, tran).T
+
+
+''' the raw fast api '''
+
+def up_arm(points, rot, rot_z, tran):
+  '''
+  points has shape (3, n)
+
+  points = np.array([pt1, pt2, pt3, ...]).T
+  '''
+  return rot.T @ rot_z.T @ points - tran[:, None]
 
 def down_arm(points, rot, rot_z, tran):
   '''
+  points has shape (3, n)
+
   points = np.array([pt1, pt2, pt3, ...]).T
   '''
   return rot_z @ rot @ (points + tran[:, None])
-
-def up_arm_single_pt(points, rot, rot_z, tran):
-  return up_arm(points[:, None], rot, rot_z, tran).reshape(-1)
-
-def up_arm_multi_pt(points, rot, rot_z, tran):
-  return up_arm(points.T, rot, rot_z, tran).T
-
-def up_arm(points, rot, rot_z, tran):
-  return rot.T @ rot_z.T @ points - tran[:, None]
